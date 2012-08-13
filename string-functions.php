6de0f68b1fb0955888df0f7384e58bd77bf707ad
Brandon Frohs <brandon@forewordreviews.com>
2012-08-13 15:28:07 -0400

    Fix to meet coding standards.

--- string-functions.php
+++ string-functions.php
@@ -2,17 +2,18 @@
 
 /**
  * Replace $limit occurences of the search string with the replacement string
- * @param mixed $search The value being searched for, otherwise known as the needle. An
- * array may be used to designate multiple needles.
- * @param mixed $replace The replacement value that replaces found search values. An
- * array may be used to designate multiple replacements.
- * @param mixed $subject The string or array being searched and replaced on, otherwise
- * known as the haystack. If subject is an array, then the search and replace is
- * performed with every entry of subject, and the return value is an array as well. 
- * @param string $count If passed, this will be set to the number of replacements
- * performed.
- * @param int $limit The maximum possible replacements for each pattern in each subject
- * string. Defaults to -1 (no limit).
+ * @param mixed $search The value being searched for, otherwise known as the
+ * needle. An array may be used to designate multiple needles.
+ * @param mixed $replace The replacement value that replaces found search
+ * values. An array may be used to designate multiple replacements.
+ * @param mixed $subject The string or array being searched and replaced on,
+ * otherwise known as the haystack. If subject is an array, then the search and
+ * replace is performed with every entry of subject, and the return value is an
+ * array as well. 
+ * @param string $count If passed, this will be set to the number of
+ * replacements performed.
+ * @param int $limit The maximum possible replacements for each pattern in each
+ * subject string. Defaults to -1 (no limit).
  * @return string This function returns a string with the replaced values.
  */
 function str_replace_limit(
@@ -21,7 +22,7 @@ function str_replace_limit(
 		$subject,
 		&$count,
 		$limit = -1
-	){
+){
 	
 	// Set some defaults
 	$count = 0;
@@ -29,30 +30,31 @@ function str_replace_limit(
 	// Invalid $limit provided. Throw a warning.
 	if(!valid_integer($limit)){
 		$backtrace = debug_backtrace();
-		trigger_error('Invalid $limit `'.$limit.'` provided to '.__function__.'() in '.
-				'`'.$backtrace[0]['file'].'` on line '.$backtrace[0]['line'].'. Expecting an '.
-				'integer', E_USER_WARNING);
+		trigger_error('Invalid $limit `'.$limit.'` provided to '.__function__.'() '.
+				'in `'.$backtrace[0]['file'].'` on line '.$backtrace[0]['line'].'. '.
+				'Expecting an integer', E_USER_WARNING);
 		return $subject;
 	}
 	
 	// Invalid $limit provided. Throw a warning.
 	if($limit<-1){
 		$backtrace = debug_backtrace();
-		trigger_error('Invalid $limit `'.$limit.'` provided to '.__function__.'() in '.
-				'`'.$backtrace[0]['file'].'` on line '.$backtrace[0]['line'].'. Expecting -1 or '.
-				'a positive integer', E_USER_WARNING);
+		trigger_error('Invalid $limit `'.$limit.'` provided to '.__function__.'() '.
+				'in `'.$backtrace[0]['file'].'` on line '.$backtrace[0]['line'].'. '.
+				'Expecting -1 or a positive integer', E_USER_WARNING);
 		return $subject;
 	}
 	
-	// No replacements necessary. Throw a notice as this was most likely not the intended
-	// use. And, if it was (e.g. part of a loop, setting $limit dynamically), it can be
-	// worked around by simply checking to see if $limit===0, and if it does, skip the
-	// function call (and set $count to 0, if applicable).
+	// No replacements necessary. Throw a notice as this was most likely not the
+	// intended use. And, if it was (e.g. part of a loop, setting $limit
+	// dynamically), it can be worked around by simply checking to see if
+	// $limit===0, and if it does, skip the function call (and set $count to 0,
+	// if applicable).
 	if($limit===0){
 		$backtrace = debug_backtrace();
-		trigger_error('Invalid $limit `'.$limit.'` provided to '.__function__.'() in '.
-				'`'.$backtrace[0]['file'].'` on line '.$backtrace[0]['line'].'. Expecting -1 or '.
-				'a positive integer', E_USER_NOTICE);
+		trigger_error('Invalid $limit `'.$limit.'` provided to '.__function__.'() '.
+				'in `'.$backtrace[0]['file'].'` on line '.$backtrace[0]['line'].'. '.
+				'Expecting -1 or a positive integer', E_USER_NOTICE);
 		return $subject;
 	}
 	
@@ -99,7 +101,8 @@ function str_replace_limit(
 		return $subject;
 		
 	} elseif(is_array($search)){
-		// Only treat $replace as an array if $search is also an array (to match str_replace())
+		// Only treat $replace as an array if $search is also an array (to match
+		// str_replace())
 		
 		// Clear keys of $search (to match str_replace()).
 		$search = array_values($search);
@@ -115,8 +118,9 @@ function str_replace_limit(
 			// Don't support multi-dimensional arrays (to match str_replace()).
 			$this_search = strval($this_search);
 			
-			// If $replace is an array, use the value of $replace[$key] as the replacement. If
-			// $replace[$key] doesn't exist, just an empty string (to match str_replace()).
+			// If $replace is an array, use the value of $replace[$key] as the
+			// replacement. If $replace[$key] doesn't exist, just an empty string (to
+			// match str_replace()).
 			if(is_array($replace)){
 				if(array_key_exists($key, $replace)){
 					$this_replace = strval($replace[$key]);
